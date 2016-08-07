b540a25bb1bce03bb4c9fac3ed1160f806cb1591
Pedro Coutinho <peco@nodesagency.com>
2016-08-07 14:10:48 +0000

    Added regular HTTP (non API) error compatibility for NodesFromRequest;

--- src/Http/Requests/NodesFormRequest.php
+++ src/Http/Requests/NodesFormRequest.php
@@ -2,10 +2,11 @@
 
 namespace Nodes\Api\Http\Requests;
 
-use Nodes\Validation\Exceptions\ValidationException;
-use Illuminate\Contracts\Validation\Validator;
+use Dingo\Api\Http\Request;
 use Dingo\Api\Http\FormRequest;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Contracts\Validation\Validator;
+use Nodes\Validation\Exceptions\ValidationException;
 
 /**
  * Class NodesApiRequest
@@ -34,7 +35,9 @@ class NodesFormRequest extends FormRequest
      *
      * @author Pedro Coutinho <peco@nodesagency.com>
      * @access public
+     *
      * @param  array $errorCodes
+     *
      * @return NodesFormRequest
      */
     public function setErrorCodes(array $errorCodes)
@@ -49,7 +52,9 @@ class NodesFormRequest extends FormRequest
      *
      * @author Pedro Coutinho <peco@nodesagency.com>
      * @access protected
+     *
      * @param \Illuminate\Contracts\Validation\Validator $validator
+     *
      * @return void
      * @throws \Nodes\Validation\Exceptions\ValidationException
      */
@@ -60,5 +65,24 @@ class NodesFormRequest extends FormRequest
         }
 
         parent::failedValidation($validator);
+    }
+
+    /**
+     * Get the proper failed validation response for the request.
+     *
+     * @param  array $errors
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function response(array $errors)
+    {
+        if (($this->ajax() && ! $this->pjax()) || $this->wantsJson()) {
+            return new JsonResponse($errors, ValidationException::VALIDATION_FAILED);
+        }
+
+        return $this->redirector->to($this->getRedirectUrl())
+            ->withInput($this->except($this->dontFlash))
+            // This makes errors display properly (original errors are placed under the key 'errors' instead of 'error')
+            ->with('error', $this->getValidatorInstance()->getMessageBag());
     }
 }
